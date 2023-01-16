b7ef98d6ab4049d7b6c3020f4c77153881421c9d
Sijita <simonjt.s2001@gmail.com>
2023-01-16 11:50:04 -0500

    Update projectCard
    responsive measures for project images

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,10 +5,8 @@ export default function ProjectCard({ screen, title, href }) {
     <a href={href} target="_blank" rel="noreferrer">
       <img
         src={screen}
-        className="p-2 m-0"
+        className="p-2 m-0 sm:w-[580px] xl:h-[360px]"
         style={{
-          width: "580px",
-          height: "360px",
           border: "1px solid #baff29",
           borderImage: "linear-gradient(45deg, #baff29 0%, #3fea2f 100%)",
           borderImageSlice: "1",
