f47d8d454f8a57d141a5a0ae8fa6193f00715891
Sijita <simonjt.s2001@gmail.com>
2023-08-08 15:29:44 -0500

    FIX: Projects list

--- src/hooks/ProjectsList.jsx
+++ src/hooks/ProjectsList.jsx
@@ -1,5 +1,4 @@
 import screen from "../images/screen.jpg";
-import screen3 from "../images/screen3.jpg";
 import screen4 from "../images/screen4.jpg";
 import screen5 from "../images/screen5.jpg";
 import screen6 from "../images/screen6.jpg";
@@ -7,12 +6,16 @@ import screen7 from "../images/screen7.jpg";
 import screen8 from "../images/screen8.jpg";
 import screen9 from "../images/screen9.jpg";
 import fullScreen2 from "../images/fullScreen4.png";
-import fullScreen3 from "../images/fullscreen3.png";
 import { useGlobalContext } from "./useGlobalContext";
 
 export default function ProjectsList() {
   const { translated } = useGlobalContext();
   const webProjects = [
+    {
+      title: "eCommerce",
+      screen: screen9,
+      href: "https://ecommerce-sijita.vercel.app/",
+    },
     {
       title: "Notys",
       screen: screen8,
@@ -41,19 +44,9 @@ export default function ProjectsList() {
       href: "https://github.com/sijita/RickAndMortyAPI_RNative",
     },
     {
-      title: "Ecommerce",
-      screen: screen3,
-      href: fullScreen3,
-    },
-    {
       title: translated ? "School page" : "Página escolar",
       screen: screen4,
       href: fullScreen2,
-    },
-    {
-      title: "eCommerce",
-      screen: screen9,
-      href: "https://ecommerce-sijita.vercel.app/",
     },
   ];
   return { webProjects };
